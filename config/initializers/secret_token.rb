9d87906e2843df801288c478d836b9d6458f52e9
Despo Pentara <despo@extractmethod.com>
2013-08-16 18:36:20 +0100

    cypriot market product price comparison

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PriceComparison::Application.config.secret_key_base = '1aaff656fdadd8df29adfcd1d6ce230134104b25ea02ac269eb174304b606b87be2bf8aa8bb3089be2378cefe5bab33eb45522f8474a0300f698b4cb370123f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PriceComparison::Application.config.session_store :cookie_store, key: '_price-comparison_session'
